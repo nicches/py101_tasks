6b556f49163c7dc4a93f8ae8baee12c090deba82
alfiya-chesnovskaya <skaalfee@gmail.com>
2020-11-08 00:16:45 +0300

    Done

--- guess_num.py
+++ guess_num.py
@@ -16,3 +16,36 @@
 
 if __name__ == '__main__':
     pass
+
+
+
+print ('Welcome! Please guess the number!')
+import random
+any_number = random.randint(1, 1000000)
+entered_number = input('Please, guess the number: ')
+
+while entered_number != ( '','exit' ) :
+    if entered_number == "" or entered_number == "exit":
+        print("Wrong")
+        break
+
+    if not entered_number.isdigit():
+        print("Please, insert the number")
+        continue
+
+    try entered_number = int(entered_number)
+
+    if entered_number < 1 or entered_number > 1000000:
+        print("Number isn't in range")
+        continue
+
+    if entered_number == any_number:
+        print('You are Right!!!')
+        break
+ 
+    elif entered_number < any_number:
+        print('The number is greater')
+    else:
+        print('The number is less')
+
+entered_number = input('Please, guess the number: ')
